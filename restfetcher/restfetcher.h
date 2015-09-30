6b517628f1f39309022c60bfdb3629e752e12d76
Charles Oder <charles@oder.us>
2015-09-30 17:57:48 -0500

    Added initial project [CO]

---
+++ restfetcher/restfetcher.h
@@ -0,0 +1,19 @@
+//
+//  restfetcher.h
+//  restfetcher
+//
+//  Created by Charles Oder on 9/30/15.
+//  Copyright © 2015 Charles Oder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for restfetcher.
+FOUNDATION_EXPORT double restfetcherVersionNumber;
+
+//! Project version string for restfetcher.
+FOUNDATION_EXPORT const unsigned char restfetcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <restfetcher/PublicHeader.h>
+
+
