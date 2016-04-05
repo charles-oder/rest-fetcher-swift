521f488a7bf4ef31fff1f4db42e6a0394c98dcbf
Charles Oder <charles@oder.us>
2016-04-05 10:41:50 -0500

    Cleaned up class targets and tests

--- restfetcherTests/RestResponseCodeTests.swift
+++ restfetcherTests/RestResponseCodeTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import RestFetcher
 
 class RestResponseCodeTests: XCTestCase {
     
--- restfetcherTests/RestResponseTests.swift
+++ restfetcherTests/RestResponseTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+@testable import RestFetcher
 
 class RestResponseTests: XCTestCase {
     
